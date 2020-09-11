b6652003e6da8fafd1cc92adf42954952189da33
rmfpdlxmtidl <gwak2837@kakao.com>
2020-09-11 23:13:18 +0900

    Apollo Server (2)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { ApolloServer } from "apollo-server";
-import resolvers from "./graphql/resolvers";
-import typeDefs from "./graphql/typeDefs";
+import resolvers from "./apollo/resolvers";
+import typeDefs from "./apollo/typeDefs";
+import context from "./apollo/context";
 
 // ApolloServer는 스키마와 리졸버가 반드시 필요함
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context,
 });
 
 // listen 함수로 웹 서버 실행
