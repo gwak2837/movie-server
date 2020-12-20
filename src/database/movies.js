5a3ac5e492aa763130130ef2623a5ed45d0fb98a
곽태욱 <gwak2837@kakao.com>
2020-12-20 23:23:30 +0900

    Update movies.js

--- src/database/movies.js
+++ src/database/movies.js
@@ -19,6 +19,31 @@ const movies = [
     name: "겨울왕국2",
     rating: 7,
   },
+  {
+    id: 5,
+    name: "조제",
+    rating: 7,
+  },
+  {
+    id: 6,
+    name: "이웃사촌",
+    rating: 6,
+  },
+  {
+    id: 7,
+    name: "도굴",
+    rating: 8,
+  },
+  {
+    id: 8,
+    name: "런",
+    rating: 8,
+  },
+  {
+    id: 9,
+    name: "러브 액츄얼리",
+    rating: 9,
+  }
 ];
 
 export default movies;
